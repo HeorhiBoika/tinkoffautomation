33a4aa4f786d1b441d78a78019bf984c948d056c
HeorhiBoika <6277vk7>
2018-05-27 15:41:27 +0300

    add property for chrome

--- src/test/java/utils/DriverSettings.java
+++ src/test/java/utils/DriverSettings.java
@@ -2,13 +2,16 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverSettings {
 
     public static WebDriver chrome() {
         //TODO: Use separate class for properties
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        return new ChromeDriver();
+        return new ChromeDriver(options);
     }
 
     public static WebDriver firefox() {
